2c030fb67d910e69e4f368647f81f6c8789d9ea0
git <claytnmorais@gmail.com>
2025-04-03 00:39:08 -0300

    adicionando health check

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from app.model import init_db
 from app.route.appointment_route import AppointmentRoute
 from app.route.medication_route import MedicationRoute
+from app.route.health_check_route import HealthCheckRoute
 
 info = Info(title="Appointment API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
@@ -13,3 +14,4 @@ init_db()
 
 AppointmentRoute().init_routes(app)
 MedicationRoute().init_routes(app)
+HealthCheckRoute().init_routes(app)
